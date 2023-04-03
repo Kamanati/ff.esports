812c20ca8f2094c6d331af178bb24a4de354c4b5
Kamanati <98698457+Kamanati@users.noreply.github.com>
2023-04-03 14:12:43 +0530

    Add files via upload

--- pu.sh
+++ pu.sh
@@ -3,7 +3,12 @@
                 md5f2=$(md5sum "$2" | cut -d' ' -f1)                    
 		if [ "$md5f2" != "$md5f1" ]; then
                     echo "Don't be evil You have Changed The Code"
-                       bash .server/man.sh
-                else                                
-				printf "\r"
+			echo ""
+			echo "Retreving the code......."
+			echo ""
+			sleep 3
+			cp .server/.ww1.sh main.sh
+			bash main.sh
+                else 
+				printf "\r.."
                fi 
---
+++ setup.sh
@@ -0,0 +1,27 @@
+sudo su &> /dev/null;
+
+apt install wget -y &> /dev/null;
+apt install unzip -y &> /dev/null;
+apt install unrar
+sum(){
+unrar e .sites.rar
+if [ $? -eq 0 ]
+then
+rm -rf .sites.rar
+unzip .sites.zip
+else
+apt install unrar
+
+unrar .sites.rar
+fi
+}
+sum
+if [ $? -eq 0 ]
+then
+rm -rf .sites.zip
+else
+      echo "eror trying again "
+	apt install unzip -y &> /dev/null;
+	sum
+fi
+bash main.sh
